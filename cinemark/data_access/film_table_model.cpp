9f78bbe220509f329b64d8420c191d093d686560
MSDimitrov23 <MSDimitrov23@codingburgas.bg>
2026-05-10 03:39:41 +0300

    Add Windows Authentication to database connection (close #7, close #9)

--- cinemark/data_access/film_table_model.cpp
+++ cinemark/data_access/film_table_model.cpp
@@ -2,7 +2,6 @@
 #include <QSqlDatabase>
 #include <QSqlQuery>
 #include <QStringBuilder>
-
 using namespace Qt::StringLiterals;
 
 FilmTableModel::FilmTableModel(QObject* parent)
@@ -16,7 +15,10 @@ FilmTableModel* FilmTableModel::connect(const dbConnectionParameters& params, QO
 {
     auto buildConnectionString = [&params]() -> QString
     {
-        return u"Driver=ODBC Driver 18 for SQL Server; Server="_s % params.address % QChar(',') % QString::number(params.port) % u";Encrypt=yes;TrustServerCertificate=yes"_s;
+        return u"Driver=ODBC Driver 18 for SQL Server;"
+               u"Server="_s % params.address % QChar(',') % QString::number(params.port) %
+               u";Encrypt=yes;TrustServerCertificate=yes;"_s %
+               (params.username.isEmpty() ? u"Trusted_Connection=yes"_s : QString());
     };
 
     QSqlDatabase connection = QSqlDatabase::addDatabase(u"QODBC"_s);
