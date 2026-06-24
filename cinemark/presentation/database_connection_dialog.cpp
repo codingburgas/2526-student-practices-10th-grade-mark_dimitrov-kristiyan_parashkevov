803ea108a68fed0340ac1d7aaa04cdf69700409e
MSDimitrov23 <MSDimitrov23@codingburgas.bg>
2026-06-24 12:13:11 +0300

    Add database connection dialog initialization

--- cinemark/presentation/database_connection_dialog.cpp
+++ cinemark/presentation/database_connection_dialog.cpp
@@ -1,11 +1,32 @@
 #include "database_connection_dialog.h"
+#include "db_connection_parameters.h"
+#include <QMetaEnum>
 #include <QFormLayout>
+#include <QHBoxLayout>
+#include <QComboBox>
 #include <QLineEdit>
 #include <QSpinBox>
 
-DatabaseConnectionDialog::DatabaseConnectionDialog() {
+DatabaseConnectionDialog::DatabaseConnectionDialog()
+    : addressBox(new QLineEdit(this))
+    , portSpinBox(new QSpinBox(this))
+    , authenticationTypeBox(new QComboBox(this))
+    , usernameBox(new QLineEdit(this))
+    , passwordBox(new QLineEdit(this))
+{
+    QMetaEnum authenticationTypeEnum = QMetaEnum::fromType<database::ConnectionParameters>();
+    for (int i = 0; i < authenticationTypeEnum.keyCount(); i++)
+    {
+        authenticationTypeBox->addItem(authenticationTypeEnum.key(i));
+    }
+
+    QHBoxLayout* addressFieldsLayout = new QHBoxLayout(this);
+    addressFieldsLayout->addWidget(addressBox);
+    addressFieldsLayout->addWidget(portSpinBox);
+
     QFormLayout* formLayout = new QFormLayout(this);
-    formLayout->addRow("Name:", nameLineEdit);
-    formLayout->addRow("Email:", emailLineEdit);
-    formLayout->addRow("Age:", ageSpinBox);
+    formLayout->addRow("Address:", addressFieldsLayout);
+    formLayout->addRow("Authentication type:", authenticationTypeBox);
+    formLayout->addRow("Username:", usernameBox);
+    formLayout->addRow("Password:", passwordBox);
 }
--- cinemark/presentation/database_connection_dialog.h
+++ cinemark/presentation/database_connection_dialog.h
@@ -1,14 +1,19 @@
 #pragma once
-#include <QDialog> // used for inheriting QDialog in the class below, shouldn't be moved to database_connection_dialog.cpp
-class QLineEdit; // forward declaring (needed for the variables below)
+#include <QDialog>
+
+class QComboBox;
+class QLineEdit;
 class QSpinBox;
 
 class DatabaseConnectionDialog : public QDialog
 {
 public:
     DatabaseConnectionDialog();
+
 private:
-    QLineEdit* nameLineEdit;
-    QLineEdit* emailLineEdit;
-    QSpinBox* ageSpinBox;
+    QLineEdit* addressBox;
+    QSpinBox* portSpinBox;
+    QComboBox* authenticationTypeBox;
+    QLineEdit* usernameBox;
+    QLineEdit* passwordBox;
 };
